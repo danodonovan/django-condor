6e9c29ad25a6aab155140efaceadab38acfec5ea
Dan O'Donovan <dan.odonovan@gmail.com>
2011-09-16 16:25:11 -0400

    More docuementation for test cases

--- condor/tests.py
+++ condor/tests.py
@@ -23,24 +23,21 @@ class ToolTest(TestCase):
 
     def test_connection_call_condor( self ):
         """ Make sure that nonsense test fails """
+        # assert that this doesn't raise an error
         (out, err) = call_condor( 'ls',  hostname=s.TEST_CONDOR_INFO['hostname']  )
 
-    def test_remote_no_env_call_condor( self ):
-        with self.assertRaises(CondorError):
-            (out, err) = call_condor( 'condor_q', hostname=s.TEST_CONDOR_INFO['hostname'] )
-
     def test_remote_hostname_call_condor( self ):
+        """ Test that the machine being connected to is itself - may fail! """
         (out, err) = call_condor( 'hostname', hostname=s.TEST_CONDOR_INFO['hostname'], env=None )
         self.assertEqual( out.strip(), s.TEST_CONDOR_INFO['hostname'] )
 
     def test_remote_call_condor( self ):
-        env = s.GRID_ENV['osg']
-        (out, err) = call_condor( 'condor_q', hostname=s.TEST_CONDOR_INFO['hostname'], env=env )
+        """ Test condor on established grid """
+        (out, err) = call_condor( 'condor_q', hostname=s.TEST_CONDOR_INFO['hostname'], env=s.GRID_ENV['osg'] )
         self.assertIn( '-- Submitter:', out )
 
-
 class CondorJobTest(TestCase):
-    """ """
+    """ Testing for the Condor django models """
 
     def test_simple_job(self):
         """ """
@@ -126,8 +123,6 @@ class CondorJobTest(TestCase):
 
         # check that we didn't time out
         self.assertEqual( j.status[0], 'Completed' )
-
-        print j.history
 
 
 
